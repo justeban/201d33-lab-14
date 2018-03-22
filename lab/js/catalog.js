0d3809080ba190b61af32c304083a20e914c18b3
Justin Morris <justinjodymorris@gmail.com>
2018-03-22 14:37:25 -0700

    stored cart to local storage

--- lab/js/catalog.js
+++ lab/js/catalog.js
@@ -2,7 +2,7 @@
 
 function populateForm() {
   //TODO: Add an <option> tag inside the form's select for each product
- 
+
 
   var itemElement=document.getElementById('items');
 
@@ -19,7 +19,8 @@ function populateForm() {
 
 function handleSubmit(event) {
   // TODO: Prevent the page from reloading
-
+  event.preventDefault();
+  // console.log(event.target.items.value);
   // Do all the things
   addSelectedItemToCart();
   saveCartToLocalStorage();
@@ -29,12 +30,15 @@ function handleSubmit(event) {
 }
 
 function addSelectedItemToCart() {
-  // TODO: Add the selected item and quantity to the cart
+  var itemName = event.target.items.value;
+  var itemQuantity = event.target.quantity.value;
+
+  new Cart(itemName, itemQuantity);
 }
 
 function saveCartToLocalStorage() {
-  // TODO: Save the cart to Local Storage
-
+  var storedCart = JSON.stringify(Cart.allContents);
+  localStorage.setItem('storedcart', storedCart );
 }
 
 function updateCounter() {
@@ -44,8 +48,9 @@ function updateCounter() {
 function updateCartPreview() {
   // TODO: Show the selected item & quantity in the cart div
 }
-
 // TODO: Put an event listener on the #catalog so that you can run the "handleSubmit" method when user submits the form (adding an item to their cart)
+var catalogSubmit = document.getElementById('catalog');
+catalogSubmit.addEventListener('submit', handleSubmit);
 
 
 // Start it up ...
